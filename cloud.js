e66a5d70d3babaa67c7a4c19c0c63788e4e19b0f
CHEN Qing <chenqing1213@gmail.com>
2017-05-15 15:02:14 +0800

    update functions with request

--- cloud.js
+++ cloud.js
@@ -1,40 +1,38 @@
 var AV = require('leanengine');
+var request = require('request');
 
-AV.Cloud.define('fetchCityRanking', function (request, response) {
-  AV.Cloud.httpRequest({
-    method: 'GET',
-    url: 'http://www.pm25.in/api/querys/aqi_ranking.json?token=RQCxTwiUMjhhcL4k3S3a',
-    success: function (httpResponse) {
+AV.Cloud.define('fetchCityRanking', function (req, res) {
+  request('http://www.pm25.in/api/querys/aqi_ranking.json?token=RQCxTwiUMjhhcL4k3S3a', function (error, response, body) {
+    if (!error) {
       var CityRanking = AV.Object.extend('CityRanking');
       var cityRanking = new CityRanking();
-      cityRanking.set('data', httpResponse.text);
+      cityRanking.set('data', body);
       cityRanking.save().then(function (cityRanking) {
         console.log('New object created with objectId: ' + cityRanking.id);
-        response.success('ok');
+        res.success("ok");
       }, function (error) {
         console.error('Failed to create new object, with error message: ' + error.message);
       });
-    },
-    error: function (httpResponse) {
-      console.error('Request failed with response code ' + httpResponse.status);
+
+    } else {
+      console.error('Request failed with error response ' + error);
     }
   });
-})
+});
 
-AV.Cloud.define('searchByLocation', function (request, response) {
+
+AV.Cloud.define('searchByLocation', function (req, res) {
+
   var getCityNamePromise = new Promise(function (resolve, reject) {
-    AV.Cloud.httpRequest({
-      method: 'GET',
-      url: "http://apis.map.qq.com/ws/geocoder/v1/?location=" + request.params.lat + "," + request.params.lng + "&key=XBMBZ-W37A4-LFPUE-D55AO-RSLZK-HMBJ6",
-      success: function (httpResponse) {
-        var cityName = JSON.parse(httpResponse.text).result.address_component.city.replace("市", "");
+    request("http://apis.map.qq.com/ws/geocoder/v1/?location=" + req.params.lat + "," + req.params.lng + "&key=XBMBZ-W37A4-LFPUE-D55AO-RSLZK-HMBJ6", function (error, response, body) {
+      if (!error) {
+        var cityName = JSON.parse(body).result.address_component.city.replace("市", "");
         resolve(cityName);
-      },
-      error: function (httpResponse) {
-        console.error('Request failed with response code ' + httpResponse.status);
-      }
+      } else
+        console.error('Request failed with response: ' + error);
     });
   });
+
 
   var fetchCityAQIPromise = new Promise(function (resolve, reject) {
 
@@ -60,9 +58,9 @@ AV.Cloud.define('searchByLocation', function (request, response) {
 
   fetchCityAQIPromise.then(function (result) {
     console.log("Fetched CityAQI Data from storage with id:" + result.id);
-    response.success(result.get('data'));
+    res.success(result.get('data'));
   }).catch(function (cityName) {
-    AV.Cloud.httpRequest({
+    request({
       method: 'GET',
       url: encodeURI("http://www.pm25.in/api/querys/aqi_details.json?city=" + cityName + "&token=RQCxTwiUMjhhcL4k3S3a"),
       success: function (httpResponse) {
@@ -75,13 +73,32 @@ AV.Cloud.define('searchByLocation', function (request, response) {
         }, function (error) {
           console.error('Failed to create new cityAQI, with error message: ' + error.message);
         });
-        response.success(httpResponse.text);
+        res.success(httpResponse.text);
       },
       error: function (httpResponse) {
         console.error('Request failed with response code ' + httpResponse.status);
       }
     });
+
+    request(encodeURI("http://www.pm25.in/api/querys/aqi_details.json?city=" + cityName + "&token=RQCxTwiUMjhhcL4k3S3a"), function (error, response, body) {
+      if (!error) {
+        var CityAQI = AV.Object.extend('CityAQI');
+        var cityAQI = new CityAQI();
+        cityAQI.set('cityName', cityName);
+        cityAQI.set('data', body);
+        cityAQI.save().then(function (cityAQI) {
+          console.log('New ' + cityName + ' cityAQI created with objectId: ' + cityAQI.id);
+        }, function (error) {
+          console.error('Failed to create new cityAQI, with error message: ' + error.message);
+        });
+        res.success(body);
+
+      } else {
+        console.error('Request failed with response: ' + error);
+      }
+
+    })
   })
-})
+});
 
 module.exports = AV.Cloud;
